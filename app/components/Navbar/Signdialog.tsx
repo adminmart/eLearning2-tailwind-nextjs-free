2382aad829c2291884417c6524a04461f49a1e3e
Raghav Sharma <73748243+peregrine-falcon@users.noreply.github.com>
2023-06-05 13:02:35 +0530

    ;;;

--- app/components/Navbar/Signdialog.tsx
+++ app/components/Navbar/Signdialog.tsx
@@ -56,10 +56,10 @@ const Signin = () => {
                                             <div>
                                                 <img
                                                     className="mx-auto h-12 w-auto"
-                                                    src="/assets/logo/Logo.svg"
+                                                    src="/assets/logo/logo.svg"
                                                     alt="Your Company"
                                                 />
-                                                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                                                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight">
                                                     Sign in to your account
                                                 </h2>
                                             </div>
